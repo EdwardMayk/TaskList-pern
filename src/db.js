6e37ce44ba324898d9d7825774e264c41f26c038
EdwardMayk <samaelminina322@gmail.com>
2022-06-21 16:55:28 -0500

    changes for deploy

--- src/db.js
+++ src/db.js
@@ -1,12 +1,21 @@
 const { Pool } = require('pg')
-const { db } = require('./config')
+// const { db } = require('./config')
+
+// const devConfig ({
+//   user: db.user,
+//   password: db.password,
+//   host: db.host,
+//   port: db.port,
+//   database: db.database,
+// })
+
+const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
+const proConfig = process.env.DATABASE_URL
 
 const pool = new Pool({
-  user: db.user,
-  password: db.password,
-  host: db.host,
-  port: db.port,
-  database: db.database,
+  connectionString:
+    process.env.NODE_ENV === 'production' ? proConfig : devConfig,
 })
 
 module.exports = pool
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
 
 const PORT = process.env.PORT || 4000
 
@@ -13,14 +14,22 @@ app.use(express.json())
 
 app.use(taskRoutes)
 
+app.use(express.static('./client/build'))
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+  app.use(express.static(path.join(__dirname, 'client/build')))
 }
+
+console.log(path.join(__dirname, 'client/build'))
 
 app.use((err, req, res, next) => {
   return res.json({
     message: err.message,
   })
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname), 'client/build/index.html')
 })
 
 app.listen(PORT || 4000)
