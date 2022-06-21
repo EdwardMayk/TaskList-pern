fd767f574487223fe175f6651642973d185e90e4
EdwardMayk <samaelminina322@gmail.com>
2022-06-20 22:21:13 -0500

    port-1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
-const path = require('path')
 const cors = require('cors')
+
+const PORT = process.env.PORT || 4000
 
 const taskRoutes = require('./routes/task.routes')
 const app = express()
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(express.static(path.join(__dirname, 'client/src')))
-}
-app.use('/', express.static('./client/src'))
 
 app.use(cors())
 app.use(morgan('dev'))
@@ -17,11 +13,15 @@ app.use(express.json())
 
 app.use(taskRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
+
 app.use((err, req, res, next) => {
   return res.json({
     message: err.message,
   })
 })
 
-app.listen(process.env.PORT || 4000)
+app.listen(PORT || 4000)
 console.log('Server on port 4000')
