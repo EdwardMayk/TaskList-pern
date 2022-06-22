5751916afb37ff3ad615dc4e7b4d74fb38eff05b
EdwardMayk <samaelminina322@gmail.com>
2022-06-22 14:00:16 -0500

    change db production

--- src/db.js
+++ src/db.js
@@ -1,12 +1,26 @@
 const { Pool } = require('pg')
-const { db } = require('./config')
+
+const isProduction = process.env.NODE_ENV === 'production'
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 
 const pool = new Pool({
-  user: db.user,
-  password: db.password,
-  host: db.host,
-  port: db.port,
-  database: db.database,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 })
 
-module.exports = pool
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+}
+
+// const { Pool } = require('pg')
+// const { db } = require('./config')
+
+// const pool = new Pool({
+//   user: db.user,
+//   password: db.password,
+//   host: db.host,
+//   port: db.port,
+//   database: db.database,
+// })
+
+// module.exports = pool
